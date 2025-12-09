fc70570bee593f21407bcb564925a5c281a779e1
Keylan King <kingkey0101@outlook.com>
2025-12-09 16:09:25 -0600

    stocks

--- questions/stocks/index.js
+++ questions/stocks/index.js
@@ -1,19 +1,49 @@
 /** Stock Trading
- * 
+ *
  * Given an array of 'prices', find the transaction that will yield the greatest profits.
- * You want to maximize your profit by choosing a single day to buy one stock and choosing 
+ * You want to maximize your profit by choosing a single day to buy one stock and choosing
  * a different day in the future to sell that stock.
- * 
+ *
  * @examples
  * stocks([8, 3, 6, 1, 6, 4, 7]) -> 6
  * stocks([1, 8, 3, 4]) -> 7
  * stocks([2, 9, 18, 9, 2]) ->  16
  */
 
-const stocks = (prices) => {
-   
-}
+//my first try(failed all)
+// const stocks = (prices) => {
+//   let l = 0;
+//   let r = 1;
+//   let maxProfit = 0
 
+//   while (l < r) {
+//     if (prices > maxProfit) {
+//         maxProfit = r;
+//       r++;
+//     } else if (prices < maxProfit) {
+//       l === r;
+//       r + 1
+//     } else {
+//       return maxProfit;
+//     }
+
+//   }
+// };
+const stocks = (prices) => {
+  let l = 0;
+  let r = 1
+  let maxProfit = 0;
+  while(r < prices.length) {
+    const profit = prices[r] - prices[l];
+    if (profit > 0) {
+      maxProfit = Math.max(maxProfit, profit);
+    } else {
+      l = r;
+    }
+    ++r
+  }
+  return maxProfit
+};
 
 //DO NOT EDIT BELOW THIS LINE
 module.exports = stocks;
