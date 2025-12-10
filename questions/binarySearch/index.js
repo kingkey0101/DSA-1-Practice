1afd1ee860b4b056b3586cdd5718f01cecdc46cb
Keylan King <kingkey0101@outlook.com>
2025-12-10 16:31:12 -0600

    binarySearch

--- questions/binarySearch/index.js
+++ questions/binarySearch/index.js
@@ -1,20 +1,51 @@
 /** Binary Search **
- * 
+ *
  * Given a sorted array 'nums' and an integer 'target', return
- * the index of the target. 
- * 
- * Note: Your solutions' time complexity must be faster than O(n). 
- * 
+ * the index of the target.
+ *
+ * Note: Your solutions' time complexity must be faster than O(n).
+ *
  * @example
  * binarySearch([1, 2, 4, 9, 12], 9) -> 3
  * binarySearch([-2, -1, 4, 5, 7], -1) -> 1
  * binarySearch([-1, 4, 18, 20], 18) -> 2
- * 
+ *
  */
 
+//my first try alone:
+// const binarySearch = (nums, target) => {
+//   let l = 0;
+//   let r = nums.length - 1;
+//   let mid = (l + r) / 2;
+
+//   if (mid < target) {
+//     let l = mid[i + 1];
+//     let mid = (l + r) / 2;
+//   }
+//   if (mid > target) {
+//     let r = mid[i - 1];
+//     let mid = (l + r) / 2;
+//   }
+//   return target
+
+// };
+
 const binarySearch = (nums, target) => {
+  let l = 0;
+  let r = nums.length - 1;
 
+  while (l <= r) {
+    let mid = Math.floor((l + r) / 2);
 
-}
+    if (nums[mid] > target) {
+      r = mid - 1;
+    } else if (nums[mid] < target) {
+      l = mid + 1;
+    }
+    else {
+        return mid;
+    }
+  }
+};
 
 module.exports = binarySearch;
