5903e967e6b62d91231a3a3a137dcc0a3e7b522d
Keylan King <kingkey0101@outlook.com>
2025-12-09 21:06:27 -0600

    productExceptSelf

--- playground.js
+++ playground.js
@@ -1,57 +1,18 @@
-// // const nums = [0, 1, 2, 3, 4];
-// // //find all subarrays in given array:
-// // for (i = 0; i < nums.length; ++i) {
-// //   console.log(`iteration ${i}`)
-// //   for (let j = i + 1; j <= nums.length; ++j) { // use <= so the final index is included(4 in this case.)
-// //     console.log(nums.slice(i, j));
-// //   }
-// // }
+const nums = [1, 2, 3, 4];
 
-// // const maxSubArray = (nums) => {
-//   let nums = ([5, 4, -1, 7, 8])
-// if(nums.length === 0) return 0;
-// let currentMax = nums[0]
-// let globalMax = nums[0]
+const leftProducts = [];
 
-// for (let i = 1; i < nums.length; ++i){
-//   currentMax = Math.max(nums[i], currentMax + nums[i])
-//   globalMax = Math.max(globalMax, currentMax)
-//   console.log(currentMax, globalMax)
-// }
-// return globalMax
-// // }
+leftProducts[0] = 1;
 
-const nums = [-2, -2, 0, 0, 1, 1, 2, 2]
+for (let i = 1; i < nums.length; ++i) {
+  leftProducts[i] = nums[i - 1] * leftProducts[i - 1];
+}
+console.log(leftProducts);
 
+const rightProducts = [];
+rightProducts[nums.length - 1] = 1;
+for (let j = nums.length - 2; j >= 0; --j) {
+  rightProducts[j] = nums[j + 1] * rightProducts[j + 1];
+}
+console.log(rightProducts);
 
-  nums.sort((a, b) => a - b);
-  const results = [];
-
-  for (let i = 0; i < nums.length - 2; ++i) {
-    if(nums[i] === nums[i -1]){
-      continue;
-    }
-    let l = i + 1;
-    let r = nums.length - 1;
-
-    while(l < r){
-
-
-    const sum = nums[i] + nums[l] + nums[r];
-    if (sum > 0) {
-      r--;
-    }
-    if (sum < 0) {
-      l++;
-    }
-    if (sum === 0) {
-      results.push([nums[i], nums[l], nums[r]]);
-      l++
-      while(nums[l] === nums[l - 1] && l < r){
-        l++
-      }
-    }
-  }
-    }
-
-console.log(results)
--- questions/productExceptSelf/index.js
+++ questions/productExceptSelf/index.js
@@ -13,8 +13,46 @@
  * productExceptSelf([10, 1, 5, 2]) -> [10, 100, 20, 50]
  */
 
-const productExceptSelf = (nums) => {
+//Solution before follow up question:
+// const productExceptSelf = (nums) => {
+// const leftProducts = []
+// const rightProducts = []
+// const results = []
 
+// leftProducts[0] = 1
+// rightProducts[nums.length - 1] = 1
+
+// for (let i = 1; i < nums.length; ++i){
+//     leftProducts[i] = leftProducts[i - 1] * nums[i -1]
+// }
+
+// for (let i = nums.length - 2; i>= 0; --i) {
+//     rightProducts[i] = rightProducts[i + 1] * nums[i +1]
+// }
+// for (let i = 0; i < nums.length; ++i){
+//     results[i] = leftProducts[i] * rightProducts[i]
+// }
+// return results
+// };
+
+// Solution after follow up question:
+
+const productExceptSelf = (nums) => {
+const results = []
+
+results[0] = 1
+
+for (let i = 1; i < nums.length; ++i){
+    results[i] = results[i - 1] * nums[i -1]
+}
+
+let rightProduct = 1
+for (let i = nums.length - 1; i >= 0; --i) {
+    results[i] = results[i] * rightProduct
+    rightProduct = rightProduct * nums[i]
+}
+
+return results
 };
 
 module.exports = productExceptSelf;
