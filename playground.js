ee3de0ef736f7a905b5893c279a98b12f3f9965b
Keylan King <kingkey0101@outlook.com>
2025-12-09 18:17:04 -0600

    threeSum

--- playground.js
+++ playground.js
@@ -1,22 +1,57 @@
-// const nums = [0, 1, 2, 3, 4];
-// //find all subarrays in given array:
-// for (i = 0; i < nums.length; ++i) {
-//   console.log(`iteration ${i}`)
-//   for (let j = i + 1; j <= nums.length; ++j) { // use <= so the final index is included(4 in this case.)
-//     console.log(nums.slice(i, j));
-//   }
-// }
+// // const nums = [0, 1, 2, 3, 4];
+// // //find all subarrays in given array:
+// // for (i = 0; i < nums.length; ++i) {
+// //   console.log(`iteration ${i}`)
+// //   for (let j = i + 1; j <= nums.length; ++j) { // use <= so the final index is included(4 in this case.)
+// //     console.log(nums.slice(i, j));
+// //   }
+// // }
 
-// const maxSubArray = (nums) => {
-  let nums = ([5, 4, -1, 7, 8])
-if(nums.length === 0) return 0;
-let currentMax = nums[0]
-let globalMax = nums[0]
+// // const maxSubArray = (nums) => {
+//   let nums = ([5, 4, -1, 7, 8])
+// if(nums.length === 0) return 0;
+// let currentMax = nums[0]
+// let globalMax = nums[0]
 
-for (let i = 1; i < nums.length; ++i){
-  currentMax = Math.max(nums[i], currentMax + nums[i])
-  globalMax = Math.max(globalMax, currentMax)
-  console.log(currentMax, globalMax)
-}
-return globalMax
+// for (let i = 1; i < nums.length; ++i){
+//   currentMax = Math.max(nums[i], currentMax + nums[i])
+//   globalMax = Math.max(globalMax, currentMax)
+//   console.log(currentMax, globalMax)
 // }
+// return globalMax
+// // }
+
+const nums = [-2, -2, 0, 0, 1, 1, 2, 2]
+
+
+  nums.sort((a, b) => a - b);
+  const results = [];
+
+  for (let i = 0; i < nums.length - 2; ++i) {
+    if(nums[i] === nums[i -1]){
+      continue;
+    }
+    let l = i + 1;
+    let r = nums.length - 1;
+
+    while(l < r){
+
+
+    const sum = nums[i] + nums[l] + nums[r];
+    if (sum > 0) {
+      r--;
+    }
+    if (sum < 0) {
+      l++;
+    }
+    if (sum === 0) {
+      results.push([nums[i], nums[l], nums[r]]);
+      l++
+      while(nums[l] === nums[l - 1] && l < r){
+        l++
+      }
+    }
+  }
+    }
+
+console.log(results)
--- questions/threeSum/index.js
+++ questions/threeSum/index.js
@@ -11,8 +11,53 @@
  * threeSum([0, 0, 0, 0]) = [[0, 0, 0]]
  */
 
+//my first try failed
+// function threeSum(nums) {
+//   let l = nums + 1;
+//   let r = nums.length - 1;
+//   for (let i = 0; i < nums.length; ++i){
+//     while (r < nums.length){
+//         const sum = nums[l] + nums[r]
+//         if(l < r){
+//             return sum[l, r]
+//         }
+//         else if(l > r){
+//             r--
+//         }
+//     }
+//     l++
+//   }
+// }
+
 function threeSum(nums) {
-  
+  nums.sort((a, b) => a - b);
+  const results = [];
+
+  for (let i = 0; i < nums.length - 2; ++i) {
+    if (nums[i] === nums[i - 1]) {
+      continue;
+    }
+    let l = i + 1;
+    let r = nums.length - 1;
+
+    while (l < r) {
+      const sum = nums[i] + nums[l] + nums[r];
+      if (sum > 0) {
+        r--;
+      }
+      if (sum < 0) {
+        l++;
+      }
+      if (sum === 0) {
+        results.push([nums[i], nums[l], nums[r]]);
+        l++;
+        while (nums[l] === nums[l - 1] && l < r) {
+          l++;
+        }
+      }
+    }
+  }
+  return results;
 }
 
 //DO NOT EDIT BELOW THIS LINE
